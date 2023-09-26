67956b72ef9af3c057359f14570022367b068dee
Kunal Samanta <kunalsamanta5@gmail.com>
2023-09-26 23:01:12 +0530

    freecomdecamp from

---
+++ index.js
@@ -0,0 +1,26 @@
+//console.log(parseInt(false));
+//console.log(Number(false));
+ //var num1=10;
+ //var result=num1>50?(num1+10)<100?(num1+20):(num1+50)<200?(num1+500):(num1+200):num1;
+ //console.log(result)\
+
+
+// var num1= 10;
+// var num2=20;
+// var num3=30;
+// var sum1= ++num1+num2++ + num3;
+// var sum2=num1++ + ++num2 + ++num3;
+// var sum3= num1++ + num2++ + num3++;
+// console.log(sum1+sum2+sum3);
+
+// var output= function (x){
+//     delete x;
+//     return x;
+// }
+// console.log(output(9));
+
+var dummy= true;
+console.log(dummy+0);
+console.log(dummy+"XYZ");
+console.log(dummy+true);
+console.log(dummy+false);
